f06f696ae62d4f258b0be7b453f302dbf8691e3e
Charlie Lamb <charlielamb20@icloud.com>
2026-03-14 11:01:52 +0000

    chore: add databuddy

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { Databuddy } from "@databuddy/sdk/react";
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      {children}
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        {children}
+      </ThemeProvider>
+      <Databuddy
+        clientId="a6084218-0c31-4378-8cc8-bfb258bd6347"
+        trackErrors
+        trackWebVitals
+      />
+    </>
   );
 }
